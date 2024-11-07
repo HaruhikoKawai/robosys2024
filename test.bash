7a37e3305bd915ebf99d41407b620174878587be
Haruhiko Kawai <mertris@outlook.jp>
2024-11-07 12:31:35 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,16 +1,20 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Kawai Haruhiko
+# SPDX-License-Identifier: BSD-3-Clause
 
-
-
-ng () {
-	echo ${1}の解釈が違うよ
+ng(){
+	echo ${1}行目が違うよ
 	res=1
 }
-res=0
-a=ゆずきず☆
-[ "$a" = ゆずきず♡  ] || ng "$LINENO"
-[ "$a" = ゆずきず☆  ] || ng "$LINENO"
 
-exit "$res"
+res=0
+
+out=$(seq 5 | ./plus_stdin)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+
+exit $res
+
 
 
